0063e7578226fd22c419a266a98eee098d2d902a
leonard freds morin <leonardfredsmorin3@gmail.com>
2024-05-25 14:19:58 +0700

    fix navbar

--- src/app/component/layouts/Navbar/index.js
+++ src/app/component/layouts/Navbar/index.js
@@ -1,27 +1,59 @@
-// components/Navbar.js
-
-import Link from "next/link";
+"use client"
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTiktok, faYoutube, faGlobe } from '@fortawesome/free-solid-svg-icons';
 import styles from './style.module.css';
 
 const Navbar = () => {
-    return (
-        <header className={styles.navbar}>
-            <nav>
-                <ul className={styles.navList}>
-                    <li className={styles.navItem}>
-                        <Link href="/">
-                            <p className={styles.navLink}>Home</p>
-                        </Link>
-                    </li>.
-                    <li className={styles.navItem}>
-                        <Link href="/magazine">
-                            <p className={styles.navLink}>Magazine</p>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className={styles.navbar}>
+      <nav className={styles.nav}>
+        <div className={styles.logo}>
+            <a href=''>e-Magazine</a>
+        </div>
+        <ul className={`${styles.navList} ${isOpen ? styles.active : ''}`}>
+          <li className={styles.navItem}>
+              <a className={styles.navLink}>Home</a>
+          </li>
+          <li className={styles.navItem}>
+              <a className={styles.navLink}>Magazine</a>
+          </li>
+          <li className={styles.navItem}>
+              <a className={styles.navLink}>Home</a>
+          </li>
+          <li className={styles.navItem}>
+              <a className={styles.navLink}>Magazine</a>
+          </li>
+          {/* Tambahkan tautan lain di sini */}
+        {isOpen && (
+            <div className={styles.icons}>
+              <a href=''><FontAwesomeIcon icon={faGlobe} /></a>
+              <a href=''><FontAwesomeIcon icon={faGlobe} /></a>
+              <a href=''><FontAwesomeIcon icon={faGlobe} /></a>
+            </div>
+          
+        )}
+        </ul>
+        <div className={styles.burger}>
+            <div className={styles.burgerContent}>
+                <div className={styles.socialLinks}>
+                <a className={styles.loginLink}>Login</a>
+                </div>
+                <div onClick={toggleMenu}>
+                <FontAwesomeIcon icon={faBars} />
+                </div>
+            </div>
+        </div>
+
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
--- src/app/component/utils/carousel/Featured_slide.js
+++ src/app/component/utils/carousel/Featured_slide.js
@@ -1,15 +1,24 @@
 "use client"
 import { useState } from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+import styles from './style.module.css'; // Pastikan menyesuaikan dengan nama file CSS Anda
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const Featured_slide = () => {
     const banners = [
         { id: 1, content1: "Certainly! Here's the revised and cleaned-up code for the carousel component Column 1", content2: "Banner 1 Column 2", bgColor: "bg-white" },
-        { id: 2, content1: "Certainly! Here's the revised and cleaned-up code for the carousel component Column 2", content2: "Banner 2 Column 2", bgColor: "bg-white" },
-        { id: 3, content1: "Certainly! Here's the revised and cleaned-up code for the carousel component Column 3", content2: "Banner 3 Column 2", bgColor: "bg-white" },
-        { id: 4, content1: "Certainly! Here's the revised and cleaned-up code for the carousel component Column 4", content2: "Banner 4 Column 2", bgColor: "bg-white" },
+        { id: 2, content1: "Certainly! Here's the revised and cleaned-up code for the carousel component Column 2", content2: "Banner 2 Column 2", bgColor: "bg-red" },
+        { id: 3, content1: "Certainly! Here's the revised and cleaned-up code for the carousel component Column 3", content2: "Banner 3 Column 2", bgColor: "bg-green" },
+        { id: 4, content1: "Certainly! Here's the revised and cleaned-up code for the carousel component Column 4", content2: "Banner 4 Column 2", bgColor: "bg-yellow" },
     ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleChangeIndex = (index) => {
+        setCurrentIndex(index);
+    };
 
     const prevSlide = () => {
         const newIndex = currentIndex === 0 ? banners.length - 1 : currentIndex - 1;
@@ -23,38 +32,29 @@ const Featured_slide = () => {
 
     return (
         <section>
-            <div className="relative w-full mx-auto">
-            <div className="overflow-hidden relative h-64" style={{ height: '700px' }}>
-                {banners.map((banner, index) => (
-                    <div
-                        key={banner.id}
-                        className={`absolute inset-0 transition-transform duration-500 ease-in-out transform ${index === currentIndex ? 'translate-x-0' : 'translate-x-full'} ${index === (currentIndex - 1 + banners.length) % banners.length ? '-translate-x-full' : ''}`}
-                        style={{ display: index === currentIndex || index === (currentIndex - 1 + banners.length) % banners.length || index === (currentIndex + 1) % banners.length ? 'block' : 'none' }}
-                    >
-                        <div className={`w-full h-full flex flex-col sm:flex-row items-center justify-center ${banner.bgColor} text-2xl p-4`}>
-                            <div className="w-full sm:w-1/2 p-2 text-dark">
-                                <h1>{banner.content1}</h1>
-                            </div>
-                            <div className="w-full sm:w-1/2 p-2 text-center">
-                                <img src="https://r.prcdn.co/res/en-us/g3416/t453408066/2/images/facelift-landing-hero-en@2x.webp" alt="Banner Image" />
+            <div className={`relative w-full mx-auto ${styles.featuredSlide}`}>
+                <AutoPlaySwipeableViews
+                    index={currentIndex}
+                    onChangeIndex={handleChangeIndex}
+                    enableMouseEvents
+                    interval={10000} // Ganti dengan interval auto play yang diinginkan (dalam milidetik)
+                >
+                    {banners.map((banner, index) => (
+                        <div key={banner.id} className={`${styles.slide} ${banner.bgColor}`}>
+                            <div className={`w-full h-full flex flex-col sm:flex-row items-center justify-center text-2xl p-4`}>
+                                <div className="w-full sm:w-1/2 p-2 text-dark">
+                                    <h1>{banner.content1}</h1>
+                                </div>
+                                {/* <div className="w-full sm:w-1/2 p-2 text-center">
+                                    <img src={`https://picsum.photos/400/200?random=${index}`} alt="Banner Image" />
+                                </div> */}
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </AutoPlaySwipeableViews>
+                <button onClick={prevSlide} className={`absolute top-1/2 left-0 transform -translate-y-1/2 bg-dark-600 text-dark px-4 py-2 rounded-r-lg focus:outline-none`}>Prev</button>
+                <button onClick={nextSlide} className={`absolute top-1/2 right-0 transform -translate-y-1/2 bg-dark-600 text-dark px-4 py-2 rounded-l-lg focus:outline-none`}>Next</button>
             </div>
-            <button
-                onClick={prevSlide}
-                className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-dark-600 text-white px-4 py-2 rounded-r-lg focus:outline-none"
-            >
-                Prev
-            </button>
-            <button
-                onClick={nextSlide}
-                className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-dark-600 text-white px-4 py-2 rounded-l-lg focus:outline-none"
-            >
-                Next
-            </button>
-        </div>
         </section>
     );
 };
